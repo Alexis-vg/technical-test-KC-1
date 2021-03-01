cbddf807a91f6128532be45844c6bab0f82ad94f
unknown <alexis.svalderrama@gmail.com>
2021-03-01 07:50:26 -0500

    Fixed typos, relative routes, and session button

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //CHARGED IMAGES
 const images = [];
-images[0] = "/assets/images/Foto1.png";
-images[1] = "/assets/images/Foto2.png";
-images[2] = "/assets/images/Foto3.png";
-images[3] = "/assets/images/Foto4.png";
+images[0] = "assets/images/Foto1.png";
+images[1] = "assets/images/Foto2.png";
+images[2] = "assets/images/Foto3.png";
+images[3] = "assets/images/Foto4.png";
 
 const imagesx2 = [];
-imagesx2[0] = "/assets/images/Foto1_2x.png";
-imagesx2[1] = "/assets/images/Foto2_2x.png";
-imagesx2[2] = "/assets/images/Foto3_2x.png";
-imagesx2[3] = "/assets/images/Foto4_2x.png";
+imagesx2[0] = "assets/images/Foto1_2x.png";
+imagesx2[1] = "assets/images/Foto2_2x.png";
+imagesx2[2] = "assets/images/Foto3_2x.png";
+imagesx2[3] = "assets/images/Foto4_2x.png";
 
 //PROCESS TO MOUNT THE IMAGES
 const slides = document.getElementById("slides");
@@ -24,6 +24,21 @@ source.media = "(max-width:768px)";
 const image = document.createElement("img");
 image.className = "picture-container__img";
 /* image.src = `${imagesx2[1]}`; */
+// Defining variables for show and hide the password when the "eye icon" is clicked
+const showHidePassword = document.getElementById("show/hide-password");
+
+const nodeToReplacePassword = document.querySelector(".hide-pass-logo");
+
+const newNodePassword = document.createElement("span");
+newNodePassword.className = "show-pass-logo";
+
+const nodeToReplaceType = document.querySelector(".main-login__password-input");
+
+const newNodeType = document.createElement("input");
+newNodeType.className = "main-login__password-input";
+newNodeType.type = "text";
+//CREATING THE VALIDATION LOGUIN WHEN THE BUTTON IS CLICKED
+const button = document.querySelector(".session-btn");
 //add the piece of code each "x" seconds recreating slider
 let index = 0;
 cambiarImagen = () => {
@@ -40,25 +55,11 @@ cambiarImagen = () => {
 
 setInterval(cambiarImagen, 2500);
 
-// Defining variables for show and hide the password when the "eye icon" is clicked
-const showHidePassword = document.getElementById("show/hide-password");
-
-const nodeToReplacePassword = document.querySelector(".hide-pass-logo");
-
-const newNodePassword = document.createElement("span");
-newNodePassword.className = "show-pass-logo";
-
-const nodeToReplaceType = document.querySelector(".main-login__password-input");
-
-const newNodeType = document.createElement("input");
-newNodeType.className = "main-login__password-input";
-newNodeType.type = "text";
-
 //CREATING THE EVENTS
-passwordShowed = (event) => {
+passwordShowed = () => {
   showHidePassword.replaceChild(newNodePassword, nodeToReplacePassword);
   showHidePassword.replaceChild(newNodeType, nodeToReplaceType);
-  console.log(event);
+  console.log();
 };
 passwordHidded = () => {
   showHidePassword.replaceChild(nodeToReplacePassword, newNodePassword);
@@ -66,9 +67,6 @@ passwordHidded = () => {
 };
 nodeToReplacePassword.addEventListener("click", passwordShowed);
 newNodePassword.addEventListener("click", passwordHidded);
-
-//CREATING THE VALIDATION LOGUIN WHEN THE BUTTON IS CLICKED
-const button = document.querySelector("button");
 accion = () => {
   valor = document.querySelector(".main-login__email-input").value;
   emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
